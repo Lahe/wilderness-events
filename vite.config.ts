3ea6c269c38a2353a05821ab70e693a3436987f9
Spree <34194182+Lahe@users.noreply.github.com>
2022-10-25 20:50:20 +0300

    Fix vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wilderness-events',
+  base: '/wilderness-events/',
   plugins: [react()],
   resolve: {
     alias: {
